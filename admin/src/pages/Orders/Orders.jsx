ea56e4575ac9aad27036d4e4fca81311d1311610
Muhammad Juraij P <127486266+MuhammadJuraij@users.noreply.github.com>
2024-11-12 22:16:13 +0530

    Update Orders.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -8,7 +8,7 @@ import {assets} from '../../assets/assets'
 
 
 function Orders() {
-  const url='http://localhost:4000'
+  const url='https://online-food-delivery-website-e-commerce.onrender.com'
 
   const [orders,setOrders]=useState([]);
 

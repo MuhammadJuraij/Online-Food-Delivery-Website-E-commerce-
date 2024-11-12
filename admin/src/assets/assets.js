679825ae877166f437448f6502b3eb3dd657568b
Muhammad Juraij P <127486266+MuhammadJuraij@users.noreply.github.com>
2024-11-12 22:15:00 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-app-backend-omd9.onrender.com'
+export const url = 'https://online-food-delivery-website-e-commerce.onrender.com'

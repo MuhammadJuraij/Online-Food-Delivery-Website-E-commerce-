d5ac977ff5cf693104f94b3463dbb51eadc173f5
Muhammad Juraij P <127486266+MuhammadJuraij@users.noreply.github.com>
2024-11-12 22:14:05 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify'
 
 function Add() {
 
-    const url = 'http://localhost:4000';
+    const url = 'https://online-food-delivery-website-e-commerce.onrender.com';
 
 
     const [image,setImage]=useState(false);

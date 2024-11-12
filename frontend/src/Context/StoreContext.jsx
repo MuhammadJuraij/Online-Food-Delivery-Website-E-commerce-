70749b5b62d443ea9fe28630401da8e6ca14ca83
Muhammad Juraij P <127486266+MuhammadJuraij@users.noreply.github.com>
2024-11-12 22:12:54 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext=createContext(null)
 
 const StoreContextProvider=(props)=>{
 
-    const url='http://localhost:4000';
+    const url='https://online-food-delivery-website-e-commerce.onrender.com';
     const [cartItem,setCartItem]=useState({})
     const [token,setToken]=useState('');
     const [food_list,setFoodlist]=useState([]);
